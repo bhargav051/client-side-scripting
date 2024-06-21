510488591d5b3be2e537b973386a31b926bee5e1
root <bhargav051>
2024-06-21 12:41:35 +0530

    first commit

---
+++ program_management/program/doctype/program_/program_.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Bhargav and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Program_(Document):
+	pass
---
+++ program_management/program/doctype/program_/test_program_.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Bhargav and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestProgram_(FrappeTestCase):
+	pass
